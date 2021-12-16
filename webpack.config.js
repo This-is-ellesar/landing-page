cb89d94f20e07c5294a1b2e6be2f6262d2cee0ce
This-is-ellesar <armen16091998@icloud.com>
2021-12-16 22:11:50 +0300

    Настроил sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+// libs
 const path = require('path');
+// plugins
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// loaders
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -13,5 +19,30 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: `./js/${filename('js')}`,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: !isDev,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename('css')}`,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
 };
