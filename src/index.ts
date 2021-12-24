4cfa61e66901e1fa776d22fe51c33956c8cb733d
This-is-ellesar <armen16091998@icloud.com>
2021-12-24 23:48:57 +0300

    Добавил анимированный бургер

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import './style/main.scss';
 
+// variables
 const burgerMenu: HTMLButtonElement = document.querySelector('#burger');
 const listMenu: HTMLElement = document.querySelector('#nav');
 
-burgerMenu.addEventListener('click', () => {
+// listeners
+burgerMenu.addEventListener('click', function (): void {
+  this.classList.toggle('opened');
   listMenu.classList.toggle('nav-active');
 });
