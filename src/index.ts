01a0b2cb907cef2274a44d6a8dfc2d7c00a06cdb
This-is-ellesar <armen16091998@icloud.com>
2021-12-25 01:23:24 +0300

    Обновил верстку

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import './style/main.scss';
+import './style/main.scss'
 
 // variables
-const burgerMenu: HTMLButtonElement = document.querySelector('#burger');
-const listMenu: HTMLElement = document.querySelector('#nav');
+const burgerMenu: HTMLButtonElement = document.querySelector('#burger')
+const listMenu: HTMLElement = document.querySelector('#nav')
+const orderDialog: HTMLDialogElement = document.querySelector('#orderDialog')
+const appointmentButton: HTMLButtonElement =
+  document.querySelector('#appointment')
 
 // listeners
 burgerMenu.addEventListener('click', function (): void {
-  this.classList.toggle('opened');
-  listMenu.classList.toggle('nav-active');
-});
+  this.classList.toggle('opened')
+  listMenu.classList.toggle('nav-active')
+})
+
+appointmentButton.addEventListener('click', () => {
+  orderDialog.classList.add('dialog--show')
+})
