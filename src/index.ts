a23c561e66cead4fc6d2a6408b8ccde950d0a077
This-is-ellesar <armen16091998@icloud.com>
2021-12-27 20:48:27 +0300

    Сверстал диалоговое окно

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,24 @@ const listMenu: HTMLElement = document.querySelector('#nav')
 const orderDialog: HTMLDialogElement = document.querySelector('#orderDialog')
 const appointmentButton: HTMLButtonElement =
   document.querySelector('#appointment')
+const closeDialog: HTMLButtonElement = document.querySelector('#closeDialog')
+const overlayDialog: HTMLDivElement = document.querySelector('#overlayDialog')
 
 // listeners
+
 burgerMenu.addEventListener('click', function (): void {
   this.classList.toggle('opened')
   listMenu.classList.toggle('nav-active')
 })
 
 appointmentButton.addEventListener('click', () => {
-  orderDialog.classList.add('dialog--show')
+  orderDialog.classList.add('dialog--active')
+})
+
+closeDialog.addEventListener('click', () => {
+  orderDialog.classList.remove('dialog--active')
+})
+
+overlayDialog.addEventListener('click', () => {
+  orderDialog.classList.remove('dialog--active')
 })
