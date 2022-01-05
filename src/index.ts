ca38f5b2a80744dc3a9d902d83c82fbf88835296
This-is-ellesar <armen16091998@icloud.com>
2022-01-05 20:01:01 +0300

    Доверстал все компоненты

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import './style/main.scss'
-// heplers
+// helpers
 import { disabledAllPanels } from './helpers/func'
 
 // variables
 const burgerMenu: HTMLButtonElement = document.querySelector('#burger')
 const listMenu: HTMLElement = document.querySelector('#nav')
-const orderDialog: HTMLDialogElement = document.querySelector('#orderDialog')
-const appointmentButton: HTMLButtonElement =
-  document.querySelector('#appointment')
+const orderDialog: HTMLElement = document.querySelector('#orderDialog')
+const priceDialog: HTMLElement = document.querySelector('#priceDialog')
+const appointmentButton: NodeListOf<HTMLElement> =
+  document.querySelectorAll('.appointment')
+const appointmentPriceButton: HTMLElement = document.querySelector('.appointment-price')
+const priceButton: HTMLElement = document.querySelector('.price')
 const closeDialog: HTMLButtonElement = document.querySelector('#closeDialog')
+const closePriceDialog: HTMLButtonElement = document.querySelector('#closePriceDialog')
 const overlayDialog: HTMLDivElement = document.querySelector('#overlayDialog')
 const navLinks = document.querySelectorAll('#nav a')
 const accordion = document.querySelectorAll('.accordion')
-const accordionArrow = document.querySelectorAll('.accordion__arrow')
 
 // listeners
-
 navLinks.forEach((link: HTMLLinkElement): void => {
   link.addEventListener('click', () => {
     orderDialog.classList.remove('dialog--active')
@@ -44,17 +46,32 @@ accordion.forEach((button) => {
   })
 })
 
+appointmentPriceButton.addEventListener('click', () => {
+  priceDialog.classList.remove('dialog--active')
+  orderDialog.classList.add('dialog--active')
+})
+
+priceButton.addEventListener('click', () => {
+  priceDialog.classList.add('dialog--active')
+})
+
 burgerMenu.addEventListener('click', function (): void {
   this.classList.toggle('opened')
   listMenu.classList.toggle('nav-active')
 })
 
-appointmentButton.addEventListener('click', (): void => {
-  orderDialog.classList.add('dialog--active')
+appointmentButton.forEach(button => {
+  button.addEventListener('click', (): void => {
+    orderDialog.classList.add('dialog--active')
+  })
 })
 
 closeDialog.addEventListener('click', (): void => {
   orderDialog.classList.remove('dialog--active')
+})
+
+closePriceDialog.addEventListener('click', (): void => {
+  priceDialog.classList.remove('dialog--active')
 })
 
 overlayDialog.addEventListener('click', (): void => {
